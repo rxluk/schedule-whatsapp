7888e335a4d981426b9661b8fb2d7ca582d9c09c
Lucas Gabriel Costa <lucasg.exe@gmail.com>
2025-10-01 14:17:51 -0300

    feat: add controllers

--- schedule-whatsapp/app/Http/Controllers/DashboardController.php
+++ schedule-whatsapp/app/Http/Controllers/DashboardController.php
@@ -7,19 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    /**
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
     public function index()
     {
         $user = Auth::user();
